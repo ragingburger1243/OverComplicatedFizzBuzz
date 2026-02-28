fdebda47ef3be4b29b5b120eca2a8ab8a00a04bc
Ragingburger <ragingburger668@gmail.com>
2026-02-28 22:37:48 +0100

    1.1 (Made a stats function)

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -25,6 +25,33 @@ def achievements():
     if count_list.count('Buzz') >= 5 and "5 BUZZ" not in achievement_list:
         achievement_list.append("5 BUZZ")
 
+def stats():
+    total = int(count_list.count("Fizz")) + int(count_list.count("Buzz")) + int(count_list.count("FizzBuzz") + int(count_list.count("Number")))
+    if total != 0:
+
+        print("=== STATS ===")
+        print(f"Total numbers: {total}")
+        print(f"Total Achievements: {len(achievement_list)}")
+        achievemnt_display = ", ".join(achievement_list)
+
+        fizz_bar = '#' * (count_list.count('Fizz') // 2)
+        number_bar = '#' * (count_list.count('Number') // 2)
+        Buzz_bar = '#' * (count_list.count('Buzz') // 2)
+        FizzBuzz_bar = '#' * (count_list.count('FizzBuzz') // 2)
+
+        print(f"Fizz:  [{fizz_bar}] [{count_list.count('Fizz')}] {(count_list.count('Fizz') / total) * 100:.2f}%")
+
+        print(f"Normal Numbers: [{number_bar}] [{count_list.count('Number')}] {(count_list.count('Number') / total) * 100:.2f}%")
+
+        print(f"Buzz: [{Buzz_bar}] [{count_list.count('Buzz')}] {(count_list.count('Buzz') / total) * 100:.2f}%")
+
+        print(f"FizzBuzz: [{FizzBuzz_bar}] [{count_list.count('FizzBuzz')}] {(count_list.count('FizzBuzz') / total) * 100:.2f}%")
+        print(f"Achievements: {achievemnt_display}")
+
+    else:
+        print("No stats found")
+
+
 
 while True:
     print("\n=== FizzBuzz Menu ===")
@@ -73,29 +100,8 @@ while True:
 
     # check your stats
     elif user_input == "4":
+        stats()
 
-        total = int(count_list.count("Fizz")) + int(count_list.count("Buzz")) + int(count_list.count("FizzBuzz") + int(count_list.count("Number")))
-        if total != 0:
-
-            print("=== STATS ===")
-            print(f"Total numbers: {total}")
-            print(f"Total Achievements: {len(achievement_list)}")
-            achievemnt_display = ", ".join(achievement_list)
-
-
-            fizz_bar = '#' * (count_list.count('Fizz') // 2)
-            number_bar = '#' * (count_list.count('Number') // 2)
-            Buzz_bar = '#' * (count_list.count('Buzz') // 2)
-            FizzBuzz_bar = '#' * (count_list.count('FizzBuzz') // 2)
-
-            print(f"Fizz:  [{fizz_bar}] [{count_list.count('Fizz')}] {(count_list.count('Fizz') / total) * 100:.2f}%")
-            print(f"Normal Numbers: [{number_bar}] [{count_list.count('Number')}] {(count_list.count('Number') / total) * 100:.2f}%")
-            print(f"Buzz: [{Buzz_bar}] [{count_list.count('Buzz')}] {(count_list.count('Buzz') / total) * 100:.2f}%")
-            print(f"FizzBuzz: [{FizzBuzz_bar}] [{count_list.count('FizzBuzz')}] {(count_list.count('FizzBuzz') / total) * 100:.2f}%")
-            print(f"Achievements: {achievemnt_display}")
-
-        else:
-            print("No stats found")
 
     # clear your stats
     elif user_input == "5":
